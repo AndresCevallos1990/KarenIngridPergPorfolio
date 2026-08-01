4e942289b83129fedd843d7c0c89b5c68cb1fda9
Andres Cevallos <ca.andres.cevallos@gmail.com>
2026-08-01 16:25:45 -0300

    Descripción de los cambios

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons"
 import 'react-toastify/dist/ReactToastify.css';
 import 'aos/dist/aos.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import CustomProvider from './Provider'
 import { ToastContainer } from 'react-toastify';
-
 import MainRenders from './GaleriaRenders/MainRenders';
 import Textos from './GaleriaRenders/Textos';
 
@@ -17,12 +16,12 @@ function App() {
   
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
       <Textos />
         <Routes>
           <Route path='/' element={<MainRenders />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <ToastContainer />
 
     </div>
