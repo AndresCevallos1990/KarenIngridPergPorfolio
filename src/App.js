7d73dcd4a4558b71334bc000b3627bec6fe5f537
Andres Cevallos <ca.andres.cevallos@gmail.com>
2026-08-01 16:31:38 -0300

    otroCambio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons"
-import 'react-toastify/dist/ReactToastify.css';
 import 'aos/dist/aos.css'
-import { HashRouter, Routes, Route } from 'react-router-dom';
 import CustomProvider from './Provider'
-import { ToastContainer } from 'react-toastify';
 import MainRenders from './GaleriaRenders/MainRenders';
 import Textos from './GaleriaRenders/Textos';
 
